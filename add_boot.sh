30436a31b44d5b771d51743f042d54bf8f7482dc
Paul Grosen <pcgrosen@gmail.com>
2018-12-05 09:56:42 -0800

    switch add_boot.sh to use systemctl instead of rc.local

--- add_boot.sh
+++ add_boot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-grep -q "avahi-daemon" /etc/rc.local || sed -i $'s/^exit 0/avahi-daemon -D || true\\\nexit 0/g' /etc/rc.local
+systemctl daemon-reload
+systemctl start avahi-daemon
+systemctl enable avahi-daemon

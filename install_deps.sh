902455750af612aa8e2d8207f81a05f44f0059ef
Paul Grosen <pcgrosen@gmail.com>
2018-12-05 09:35:16 -0800

    add dbus support

--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apt install -y gettext intltool libtool libglib2.0-dev libgdbm-dev libdaemon-dev
+apt install -y gettext intltool libtool libglib2.0-dev libgdbm-dev libdaemon-dev libdbus-1-dev
